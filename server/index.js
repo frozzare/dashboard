18cdab965e6359030d50b1d59af055a3120f5126
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2018-01-09 12:48:00 +0100

    Add http-auth support

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const fs = require('fs');
 const path = require('path');
 const glob = require('glob-fs')();
 const request = require('request');
+const auth = require('http-auth');
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
-
 const port = process.env.PORT || 3000;
+const htpasswd = path.join(__dirname, '..', '.htpasswd');
 
+// Include widgets socket files.
 io.on('connect', socket => {
   const widgets = glob.readdirSync('widgets/*');
   widgets.forEach(w => {
@@ -22,12 +24,23 @@ io.on('connect', socket => {
   });
 });
 
+// Handle next app.
 nextApp.prepare().then(() => {
+  // Proxy route for API's.
   app.get('/proxy', (req, res) => {
     req.pipe(request(req.query.url)).pipe(res);
   });
 
-  app.get('*', nextHandler);
+  // Add basic auth if htpasswd file exists.
+  if (fs.existsSync(htpasswd)) {
+    const basic = auth.basic({
+	    realm: 'Dashboard',
+	    file: htpasswd
+    });
+    app.get('*', auth.connect(basic), nextHandler);
+  } else {
+    app.get('*', nextHandler);
+  }
 
   server.listen(port, err => {
     if (err) throw err;
